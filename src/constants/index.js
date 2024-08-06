da7c447a759fa62a54cfadb50100a432970ef983
Akash M <akashm2001103199@gmail.com>
2024-08-06 22:24:19 +0530

    Akazportfolio:initial

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Highly motivated and skilled software engineer with a Master's degree in Software Engineering from Vellore Institute of Technology, graduating in June 2024. Proficient in Java, Spring Boot, and a wide array of technologies including REST API, MySQL, and Docker. Demonstrated expertise in developing secure and efficient applications through various projects such as token-based authentication systems, microservice architectures, and cloud-hosted solutions.`;
+
+export const ABOUT_TEXT = `Strong background in DevOps practices and AWS services, enhancing project efficiency and scalability. Certified in Java, Spring Boot, DevOps, and Data Analytics, with a track record of significant improvements in project security, efficiency, and performance. Seeking opportunities to leverage technical skills and innovative problem-solving abilities in a dynamic software engineering role.`;
+
+export const EXPERIENCE = [
+  {
+    year: "2023 - Present",
+    name: "Software Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "DB MySQL"],
+  },
+  {
+    year: "2022 - 2023",
+    name: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    name: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    name: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Spring Security - Token Based Authentication & Authorization Using JWT",
+    image: project1,
+    description:
+      "Constructed a robust token-based authentication system using a 3-Way security filter chain method, Signature authentication & authorization.Process increases and enhances the overall security and integrity of the application by 75%.",
+    technologies: ["HTML", "CSS", "Java", "Rest API", "H2-DB"],
+  },
+  {
+    title: "V-Profile Project Setup (Mannual & Automation)",
+    image: project2,
+    description:
+      "Organized the comprehensive stack project through a blend of manual and automated processes. reduction of human effort by up to 90%, enhancing efficiency and reducing errors.",
+    technologies: ["HTML", "CSS", "Vagrant", "Shell","Linux"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "JS", "React", "Tailwind CSS"],
+  },
+  {
+    title: "Cloud - Website hosting in AWS and Containerizing in Docker",
+    image: project4,
+    description:
+      "I employed AWS resources such as EC2, EBS, ELB, S3, and private DNS to securely host a personal blog application.Leveraging autoscaling ensured seamless maintenance, adapting resources to demand dynamically.",
+    technologies: ["AWS", "Docker", "Java", "Private DNS", "mySQL","git"],
+  },
+];
+
+export const CONTACT = {
+  address: "No,36 3rd Cross Road Azhagapuri Nagar ",
+  phoneNo: "+987654321 ",
+  email: "makash31102001@gmail.com",
+};
