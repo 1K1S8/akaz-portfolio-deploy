733582d02323f58c949524e79491442e5d02e757
Akash M <akashm2001103199@gmail.com>
2024-08-07 09:35:38 +0530

    added animation : navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,60 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import {motion} from "framer-motion";
+
+const iconVariants = (duration)=>({
+  intial: {y:-10},
+  animate: {
+    y:[10,-10],
+    transition:{
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType:"reverse",
+    },
+  },
+});
 
 const Navbar = () => {
   return( 
     <nav className=" mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo"/>
+         <img className="mx-2 w-10" src={logo} alt="logo"/>
       </div> 
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaInstagram/>
-        <FaSquareXTwitter/>
+      <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+            <FaLinkedin className="text-2xl text-blue-400"/>
+      </motion.div>
+      <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+            <FaGithub className="text-2xl text-black-400"/>
+      </motion.div>
+      <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+            <FaInstagram className="text-2xl text-pink-400"/>
+      </motion.div>
+      <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+            <FaSquareXTwitter className="text-2xl text-black-400"/>
+      </motion.div>
+        {/* <FaLinkedin/> */}
+        {/* <FaGithub/> */}
+        {/* <FaInstagram/> */}
+        {/* <FaSquareXTwitter/> */}
       </div>
   </nav>     
   );
