fe7385939e94bc2c15031124e10636972ff3ce06
Akash M <akashm2001103199@gmail.com>
2024-08-07 16:27:23 +0530

    icons :animation change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,10 +27,34 @@ const Navbar = () => {
          <img className="mx-2 w-10" src={logo} alt="logo"/>
       </div> 
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <a href="https://www.linkedin.com/in/akash-m-061aa224b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="text-2xl text-blue-400"><FaLinkedin/></a>
-        <a href="https://github.com/1K1S8"><FaGithub/></a>
-        <a href="https://www.instagram.com/accounts/login/?hl=en" className="text-2xl text-pink-400"><FaInstagram/></a>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+          <a href="https://www.linkedin.com/in/akash-m-061aa224b/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="text-2xl text-blue-400" ><FaLinkedin/></a>
+      </motion.div>
+      <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+          <a href="https://github.com/1K1S8"><FaGithub/></a>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
+          <a href="https://www.instagram.com/accounts/login/?hl=en" className="text-2xl text-pink-400"><FaInstagram/></a>
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          classname="rounded-2xl border-4 border-neutral-800 p-4">
         <a href="https://x.com/i/flow/login"><FaSquareXTwitter/></a>
+        </motion.div>
       
 
       {/* 
